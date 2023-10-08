2966def2d17d30f1fed07d93ee7927d5f9cce629
Mustafa Badr <mustafabadr879@gmail.com>
2023-10-08 22:53:14 +0300

    fix popper bug.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import HomePage from "./components/pages/homePage";
 import NotFoundPage from "./components/pages/notFoundPage";
 import SignupPage from "./components/static/signupPage";
-import viewSingleProductPage from "./components/pages/viewSingelProduct";
 import LoginModal from "./components/static/loginModal";
 import MyNavbar from "./components/static/myNavbar";
 import FavPage from "./components/pages/FavPage";
-import { useDispatch } from "react-redux";
-import { GetProductsListAction } from "./store/actions/GetProductsList";
 import SellPage from "./components/pages/sellPage";
 import UserAdsPage from "./components/pages/userAdsPage";
 import ProfilePage from "./components/pages/UserProfilePage";
