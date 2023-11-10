ada349b488b47a3d44828458e9a84d98109e6ea3
msabour11 <mohamedabdo2611@gmail.com>
2023-11-10 21:36:25 +0200

    add pagination feature

--- src/components/pages/homePage.jsx
+++ src/components/pages/homePage.jsx
@@ -1,4 +1,163 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import MyFooter from "../static/footer";
+// import { useDispatch, useSelector } from "react-redux";
+// import HeroSection from "../static/heroSection";
+// import NothingFoundAlert from "../static/nothingFoundAlert";
+// import LoadingSpinner from "../static/loadingSpinner";
+// import ListingCard from "../static/listingCard";
+// import { AddToProductListAction } from "../../store/actions/AddToProductList";
+// import RateComp from "../static/RateComp";
+// import { GetProductsListAction } from "../../store/actions/GetProductsList";
+// import { IsLoadingAction } from "../../store/actions/ISLoadingAction";
+
+// const HomePage = () => {
+//   const localIsLoading = useSelector((state) => state.IsLOADING.isLoading);
+//   const products = useSelector((state) => state.Products.productList);
+//   const isResultFound = useSelector((state) => state.Products.found);
+//   // console.log(isResultFound);
+
+//   const dispatch = useDispatch();
+  
+
+//   useEffect(() => {
+//     dispatch(IsLoadingAction(false))
+
+//   }, [products]);
+
+//   return (
+//     <>
+   
+//       <HeroSection />
+//       <div className="container mt-5">
+//         <div className="row">
+//           <div className="col-3">
+//             <h2 className="fw-bold ">Check our Listings</h2>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="container mt-5">
+//         <div className="row">
+//           {localIsLoading ? (
+//             <LoadingSpinner />
+//           ) : (
+//             products.map((item, index) => {
+//               return <ListingCard key={index} productObject={item} />;
+//             })
+//           )}
+//         </div>
+//       </div>
+
+//       {isResultFound === false && <NothingFoundAlert />}
+
+//       <MyFooter />
+//     </>
+//   );
+// };
+
+// export default HomePage;
+
+
+// pagination without next and previous buttons
+// import React, { useEffect, useState } from "react";
+// import MyFooter from "../static/footer";
+// import { useDispatch, useSelector } from "react-redux";
+// import HeroSection from "../static/heroSection";
+// import NothingFoundAlert from "../static/nothingFoundAlert";
+// import LoadingSpinner from "../static/loadingSpinner";
+// import ListingCard from "../static/listingCard";
+// import { AddToProductListAction } from "../../store/actions/AddToProductList";
+// import RateComp from "../static/RateComp";
+// import { GetProductsListAction } from "../../store/actions/GetProductsList";
+// import { IsLoadingAction } from "../../store/actions/ISLoadingAction";
+
+// const HomePage = () => {
+//   const localIsLoading = useSelector((state) => state.IsLOADING.isLoading);
+//   const products = useSelector((state) => state.Products.productList);
+//   const isResultFound = useSelector((state) => state.Products.found);
+
+//   const dispatch = useDispatch();
+//   const itemsPerPage = 6;
+//   const [currentPage, setCurrentPage] = useState(1);
+  
+
+  // Calculate the index range for the current page
+//   const indexOfLastItem = currentPage * itemsPerPage;
+//   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+//   const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+//   // Change page
+//   const handlePageChange = (pageNumber) => {
+//     setCurrentPage(pageNumber);
+//   };
+
+//   useEffect(() => {
+//     dispatch(IsLoadingAction(false));
+//   }, [products]);
+
+//   return (
+//     <>
+//       <HeroSection />
+//       <div className="container mt-5">
+//         <div className="row">
+//           <div className="col-3">
+//             <h2 className="fw-bold">Check our Listings</h2>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="container mt-5">
+//         <div className="row">
+//           {localIsLoading ? (
+//             <LoadingSpinner />
+//           ) : (
+//             currentItems.map((item, index) => (
+//               <ListingCard key={index} productObject={item} />
+//             ))
+//           )}
+//         </div>
+//       </div>
+
+//       {isResultFound === false && <NothingFoundAlert />}
+
+//       {/* Pagination controls */}
+//       <div className="container mt-3">
+//         <div className="row">
+//           <div className="col-12">
+//             <ul className="pagination">
+//               {Array.from(
+//                 { length: Math.ceil(products.length / itemsPerPage) },
+//                 (_, index) => (
+//                   <li
+//                     key={index}
+//                     className={`page-item ${
+//                       currentPage === index + 1 ? "active" : ""
+//                     }`}
+//                   >
+//                     <a
+//                       href="#!"
+//                       className="page-link"
+//                       onClick={() => handlePageChange(index + 1)}
+//                     >
+//                       {index + 1}
+//                     </a>
+//                   </li>
+//                 )
+//               )}
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+
+//       <MyFooter />
+//     </>
+//   );
+// };
+
+// export default HomePage;
+
+
+import React, { useEffect, useState } from "react";
 import MyFooter from "../static/footer";
 import { useDispatch, useSelector } from "react-redux";
 import HeroSection from "../static/heroSection";
@@ -14,24 +173,32 @@ const HomePage = () => {
   const localIsLoading = useSelector((state) => state.IsLOADING.isLoading);
   const products = useSelector((state) => state.Products.productList);
   const isResultFound = useSelector((state) => state.Products.found);
-  // console.log(isResultFound);
 
   const dispatch = useDispatch();
-  
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calculate the index range for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   useEffect(() => {
-    dispatch(IsLoadingAction(false))
-
+    dispatch(IsLoadingAction(false));
   }, [products]);
 
   return (
     <>
-   
       <HeroSection />
       <div className="container mt-5">
         <div className="row">
           <div className="col-3">
-            <h2 className="fw-bold ">Check our Listings</h2>
+            <h2 className="fw-bold">Check our Listings</h2>
           </div>
         </div>
       </div>
@@ -41,10 +208,65 @@ const HomePage = () => {
           {localIsLoading ? (
             <LoadingSpinner />
           ) : (
-            products.map((item, index) => {
-              return <ListingCard key={index} productObject={item} />;
-            })
+            currentItems.map((item, index) => (
+              <ListingCard key={index} productObject={item} />
+            ))
           )}
+        </div>
+      </div>
+
+      {/* Pagination controls */}
+      <div className="container mt-3 text-center">
+        <div className="row justify-content-center ">
+          <div className="col-12">
+            <ul className="pagination">
+              <li
+                className={`page-item ${currentPage === 1 ? "disabled" : ""}`}
+              >
+                <a
+                  href="#!"
+                  className="page-link"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                >
+                  Previous
+                </a>
+              </li>
+              {Array.from(
+                { length: Math.ceil(products.length / itemsPerPage) },
+                (_, index) => (
+                  <li
+                    key={index}
+                    className={`page-item ${
+                      currentPage === index + 1 ? "active" : ""
+                    }`}
+                  >
+                    <a
+                      href="#!"
+                      className="page-link"
+                      onClick={() => handlePageChange(index + 1)}
+                    >
+                      {index + 1}
+                    </a>
+                  </li>
+                )
+              )}
+              <li
+                className={`page-item ${
+                  currentPage === Math.ceil(products.length / itemsPerPage)
+                    ? "disabled"
+                    : ""
+                }`}
+              >
+                <a
+                  href="#!"
+                  className="page-link"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                >
+                  Next
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -56,3 +278,6 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+
